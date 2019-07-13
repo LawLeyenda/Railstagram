b57bbf5180ea5694015768057f3cfabf2eea18ab
sean <smgelbgithub@gmail.com>
2019-07-13 12:03:47 -0400

    push 3

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,5 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+  has_many :post_hash_tags
+  has_many :hash_tags, through: :post_hash_tags
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'search/index'
   devise_for :users
   get 'home/index'
   root 'home#index'
+  get 'search' => 'search#index'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #
   resources :users, only: [:show, :edit, :update]
